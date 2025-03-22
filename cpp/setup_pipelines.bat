ef698ba1d273e6c3a12dc56d1b97c03d403772d5
Easton Potokar <contagon6@gmail.com>
2025-03-22 12:44:16 -0400

    Try to fix windows and mac

---
+++ cpp/setup_pipelines.bat
@@ -0,0 +1,38 @@
+:: Call this script from the root of evalio
+@ECHO Off
+
+:: ----------------- Pipelines ----------------- ::
+:: Create the pipelines-src directory if it doesn't exist
+if not exist cpp\bindings\pipelines-src (
+    mkdir cpp\bindings\pipelines-src
+    echo "doesn't exist"
+)
+cd "cpp/bindings/pipelines-src"
+
+:: kiss
+if not exist kiss-icp (
+    git clone https://github.com/PRBonn/kiss-icp.git
+)
+cd kiss-icp
+git stash
+git switch --detach v1.2.2
+git apply ../../pipelines/kiss_icp.patch
+cd ..
+
+:: lio-sam
+if not exist LIO-SAM (
+    git clone --depth 1 https://github.com/contagon/LIO-SAM.git
+)
+cd "LIO-SAM"
+git stash
+git checkout master
+cd ..
+
+:: ------------------------- Dependencies ------------------------- ::
+cd ../../..
+if not exist .vcpkg (
+    :: Clone the vcpkg repository
+    git clone --depth 1 https://github.com/microsoft/vcpkg.git .vcpkg/
+)
+:: Bootstrap vcpkg
+.\.vcpkg\bootstrap-vcpkg.bat
