a9caf418d636f75d9107f7b7ed72d10ec5867dfd
Easton Potokar <contagon6@gmail.com>
2025-04-23 15:15:10 -0400

    Add typing cleanup fix

--- python/evalio/__init__.py
+++ python/evalio/__init__.py
@@ -1,5 +1,19 @@
 from . import _cpp, datasets, pipelines, types, utils
 
+# remove false nanobind reference leak warnings
+# https://github.com/wjakob/nanobind/discussions/13
+import atexit
+
+
+def cleanup():
+    import typing
+
+    for cleanup in typing._cleanups:  # type: ignore
+        cleanup()
+
+
+atexit.register(cleanup)
+
 __version__ = "0.2.0"
 __all__ = [
     "datasets",
