94777ec9b1c043484854d3eb31315095465ce7c1
Easton Potokar <contagon6@gmail.com>
2025-06-06 21:33:51 -0500

    Removed stable with mike 2.1.3

--- stable/ref/cli.py
+++
@@ -1,7 +0,0 @@
-from typer.cli import app
-from contextlib import redirect_stdout
-import mkdocs_gen_files
-
-with mkdocs_gen_files.open("ref/cli.md", "w") as f:
-    with redirect_stdout(f):
-        app(["evalio.cli", "utils", "docs", "--name", "evalio"])
