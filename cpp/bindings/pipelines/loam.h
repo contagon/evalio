bf730231bcd1ca501cfe1845db2532e19b349e19
Easton Potokar <contagon6@gmail.com>
2025-07-10 15:33:06 -0400

    Fix loam vcpkg issues / clang-format

--- cpp/bindings/pipelines/loam.h
+++ cpp/bindings/pipelines/loam.h
@@ -9,7 +9,7 @@
 #include "loam/loam.h"
 
 class LOAM : public evalio::Pipeline {
- public:
+public:
   LOAM() {}
 
   // Info
@@ -46,22 +46,26 @@ class LOAM : public evalio::Pipeline {
   }
 
   // Getters
-  const evalio::SE3 pose() override { return to_se3(current_estimated_pose) * lidar_T_imu_; }
+  const evalio::SE3 pose() override {
+    return to_se3(current_estimated_pose) * lidar_T_imu_;
+  }
 
   const std::vector<evalio::Point> map() override {
-    return features_to_points(transform_features(map_features(), current_estimated_pose));
+    return features_to_points(
+        transform_features(map_features(), current_estimated_pose));
   }
 
   // Setters
-  void set_imu_params(evalio::ImuParams params) override {};
+  void set_imu_params(evalio::ImuParams params) override{};
   void set_lidar_params(evalio::LidarParams params) override {
     loam_lidar_params_ =
-        std::make_unique<loam::LidarParams>(params.num_rows, params.num_columns, params.min_range, params.max_range);
+        std::make_unique<loam::LidarParams>(params.num_rows, params.num_columns,
+                                            params.min_range, params.max_range);
   };
   void set_imu_T_lidar(evalio::SE3 T) override { lidar_T_imu_ = T.inverse(); }
 
   void set_params(std::map<std::string, evalio::Param> params) override {
-    for (auto& [key, value] : params) {
+    for (auto &[key, value] : params) {
       if (std::holds_alternative<int>(value)) {
         if (key == "num_local_map_scans") {
           num_local_map_scans_ = std::get<int>(value);
@@ -95,17 +99,23 @@ class LOAM : public evalio::Pipeline {
         } else if (key == "parallel_thresh") {
           loam_fe_params_.occlusion_thresh = std::get<double>(value);
         } else if (key == "rotation_convergence_thresh") {
-          loam_registration_params_.rotation_convergence_thresh = std::get<double>(value);
+          loam_registration_params_.rotation_convergence_thresh =
+              std::get<double>(value);
         } else if (key == "position_convergence_thresh") {
-          loam_registration_params_.position_convergence_thresh = std::get<double>(value);
+          loam_registration_params_.position_convergence_thresh =
+              std::get<double>(value);
         } else if (key == "max_edge_neighbor_dist") {
-          loam_registration_params_.max_edge_neighbor_dist = std::get<double>(value);
+          loam_registration_params_.max_edge_neighbor_dist =
+              std::get<double>(value);
         } else if (key == "min_line_condition_number") {
-          loam_registration_params_.min_line_condition_number = std::get<double>(value);
+          loam_registration_params_.min_line_condition_number =
+              std::get<double>(value);
         } else if (key == "max_plane_neighbor_dist") {
-          loam_registration_params_.max_plane_neighbor_dist = std::get<double>(value);
+          loam_registration_params_.max_plane_neighbor_dist =
+              std::get<double>(value);
         } else if (key == "max_avg_point_plane_dist") {
-          loam_registration_params_.max_avg_point_plane_dist = std::get<double>(value);
+          loam_registration_params_.max_avg_point_plane_dist =
+              std::get<double>(value);
         }
       }
     }
@@ -113,15 +123,17 @@ class LOAM : public evalio::Pipeline {
 
   // Doers
   void initialize() override {}
-  void add_imu(evalio::ImuMeasurement mm) override {};
+  void add_imu(evalio::ImuMeasurement mm) override{};
 
   std::vector<evalio::Point> add_lidar(evalio::LidarMeasurement mm) override {
     // Handle Edge case of the first scan
     if (past_k_scans_.size() == 0) {
       // Extract Features from the first scan
-      auto scan_features = loam::extractFeatures(mm.points, *loam_lidar_params_, loam_fe_params_);
+      auto scan_features = loam::extractFeatures(mm.points, *loam_lidar_params_,
+                                                 loam_fe_params_);
       // Initialize the Local Map
-      past_k_scans_.push_back(std::make_pair(loam::Pose3d::Identity(), scan_features));
+      past_k_scans_.push_back(
+          std::make_pair(loam::Pose3d::Identity(), scan_features));
       // Initialize the odometry frame pose
       current_estimated_pose = loam::Pose3d::Identity();
       // Return the initial scan features
@@ -130,15 +142,21 @@ class LOAM : public evalio::Pipeline {
     // Default case for all iterations except the first
     else {
       // Extract Features from the new scan
-      auto scan_features = loam::extractFeatures(mm.points, *loam_lidar_params_, loam_fe_params_);
+      auto scan_features = loam::extractFeatures(mm.points, *loam_lidar_params_,
+                                                 loam_fe_params_);
       // Get the Local Map Features (in the frame of the previous scan)
       auto local_map = map_features();
       // Register to the local map
-      auto init_transform = past_k_scans_.size() > 0 ? past_k_scans_.back().first : loam::Pose3d();
-      auto map_T_scan = loam::registerFeatures(scan_features, local_map, init_transform, loam_registration_params_);
-      // Aggregate scan into history maintaining only K=num_local_map_scans scans
+      auto init_transform = past_k_scans_.size() > 0
+                                ? past_k_scans_.back().first
+                                : loam::Pose3d();
+      auto map_T_scan = loam::registerFeatures(
+          scan_features, local_map, init_transform, loam_registration_params_);
+      // Aggregate scan into history maintaining only K=num_local_map_scans
+      // scans
       past_k_scans_.push_back(std::make_pair(map_T_scan, scan_features));
-      if (past_k_scans_.size() > num_local_map_scans_) past_k_scans_.pop_front();
+      if (past_k_scans_.size() > num_local_map_scans_)
+        past_k_scans_.pop_front();
       // Update the Odometry frame pose
       current_estimated_pose = current_estimated_pose.compose(map_T_scan);
       // Return the points associated with the used features
@@ -146,7 +164,7 @@ class LOAM : public evalio::Pipeline {
     }
   }
 
- private:
+private:
   /// @brief The lidar params converted to the LOAM type
   std::unique_ptr<loam::LidarParams> loam_lidar_params_;
   /// @brief The parameters for feature (plan + edge) extraction
@@ -156,39 +174,50 @@ class LOAM : public evalio::Pipeline {
   /// @brief The number of scans to keep in the local map
   size_t num_local_map_scans_{1};
 
-  /// @brief The last k scans (features points only) and their respective odometry measurements
-  std::deque<std::pair<loam::Pose3d, loam::LoamFeatures<evalio::Point>>> past_k_scans_;
+  /// @brief The last k scans (features points only) and their respective
+  /// odometry measurements
+  std::deque<std::pair<loam::Pose3d, loam::LoamFeatures<evalio::Point>>>
+      past_k_scans_;
   ///  @brief The current pose estimate in the odometry frame {odom_T_lidar}
   loam::Pose3d current_estimated_pose{loam::Pose3d::Identity()};
   /// @brief The transform from lidar to IMU
   evalio::SE3 lidar_T_imu_{evalio::SE3::identity()};
 
- private:
+private:
   /// @brief Converts a loam SE(3) pose type to a evalio SE(3) pose type
   inline evalio::SE3 to_se3(loam::Pose3d pose) {
     return evalio::SE3(evalio::SO3::fromEigen(pose.rotation), pose.translation);
   }
 
   /// @brief Converts evalio point to eigen
-  inline Eigen::Vector3d to_eigen_point(evalio::Point point) { return {point.x, point.y, point.z}; }
+  inline Eigen::Vector3d to_eigen_point(evalio::Point point) {
+    return {point.x, point.y, point.z};
+  }
 
   /// @brief Transforms a evalio point from its source frame to a target frame
-  inline evalio::Point transform_point(evalio::Point src_pt, loam::Pose3d target_T_source) {
+  inline evalio::Point transform_point(evalio::Point src_pt,
+                                       loam::Pose3d target_T_source) {
     auto tgt_pt = target_T_source.act(to_eigen_point(src_pt));
     return {
-        tgt_pt(0), tgt_pt(1), tgt_pt(2), src_pt.intensity, src_pt.t, src_pt.range, src_pt.row, src_pt.col,
+        tgt_pt(0), tgt_pt(1),    tgt_pt(2),  src_pt.intensity,
+        src_pt.t,  src_pt.range, src_pt.row, src_pt.col,
     };
   }
 
-  /// @brief Helper to aggregate all features (edge + planar) into a single container
-  std::vector<evalio::Point> features_to_points(const loam::LoamFeatures<evalio::Point> features) {
+  /// @brief Helper to aggregate all features (edge + planar) into a single
+  /// container
+  std::vector<evalio::Point>
+  features_to_points(const loam::LoamFeatures<evalio::Point> features) {
     std::vector<evalio::Point> points;
-    for (const auto& planar_pt : features.planar_points) points.push_back(planar_pt);
-    for (const auto& edge_pt : features.edge_points) points.push_back(edge_pt);
+    for (const auto &planar_pt : features.planar_points)
+      points.push_back(planar_pt);
+    for (const auto &edge_pt : features.edge_points)
+      points.push_back(edge_pt);
     return points;
   }
 
-  /// @brief Aggregate the history of k most recent scans into a local map in the frame of the most recent registered
+  /// @brief Aggregate the history of k most recent scans into a local map in
+  /// the frame of the most recent registered
   loam::LoamFeatures<evalio::Point> map_features() {
     // Setup accumulators
     auto current_T_map = loam::Pose3d::Identity();
@@ -198,12 +227,18 @@ class LOAM : public evalio::Pipeline {
     for (auto it = past_k_scans_.rbegin(); it != past_k_scans_.rend(); ++it) {
       auto [prev_T_current, features] = *it;
 
-      // Transform from the current scan frame into the map frame (latest registered scan)
-      auto scan_feat_in_map_frame = transform_features(features, current_T_map.inverse());
-      map_features.edge_points.insert(map_features.edge_points.end(), scan_feat_in_map_frame.edge_points.begin(),
-                                      scan_feat_in_map_frame.edge_points.end());
-      map_features.planar_points.insert(map_features.planar_points.end(), scan_feat_in_map_frame.planar_points.begin(),
-                                        scan_feat_in_map_frame.planar_points.end());
+      // Transform from the current scan frame into the map frame (latest
+      // registered scan)
+      auto scan_feat_in_map_frame =
+          transform_features(features, current_T_map.inverse());
+      map_features.edge_points.insert(
+          map_features.edge_points.end(),
+          scan_feat_in_map_frame.edge_points.begin(),
+          scan_feat_in_map_frame.edge_points.end());
+      map_features.planar_points.insert(
+          map_features.planar_points.end(),
+          scan_feat_in_map_frame.planar_points.begin(),
+          scan_feat_in_map_frame.planar_points.end());
       // Accumulate the transforms
       current_T_map = prev_T_current.compose(current_T_map);
     }
@@ -211,14 +246,17 @@ class LOAM : public evalio::Pipeline {
   }
 
   /// @brief Transforms all features into a new frame
-  loam::LoamFeatures<evalio::Point> transform_features(loam::LoamFeatures<evalio::Point> features,
-                                                       loam::Pose3d target_T_source) {
+  loam::LoamFeatures<evalio::Point>
+  transform_features(loam::LoamFeatures<evalio::Point> features,
+                     loam::Pose3d target_T_source) {
     loam::LoamFeatures<evalio::Point> transformed_features;
-    for (const auto& planar_pt : features.planar_points) {
-      transformed_features.planar_points.push_back(transform_point(planar_pt, target_T_source));
+    for (const auto &planar_pt : features.planar_points) {
+      transformed_features.planar_points.push_back(
+          transform_point(planar_pt, target_T_source));
     }
-    for (const auto& edge_pt : features.edge_points) {
-      transformed_features.edge_points.push_back(transform_point(edge_pt, target_T_source));
+    for (const auto &edge_pt : features.edge_points) {
+      transformed_features.edge_points.push_back(
+          transform_point(edge_pt, target_T_source));
     }
     return transformed_features;
   }
