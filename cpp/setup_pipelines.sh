6a19530b0617998d36282cae345c7360bba5bc3f
Easton Potokar <contagon6@gmail.com>
2025-09-10 20:42:01 -0400

    Got mad-icp implemented

--- cpp/setup_pipelines.sh
+++ cpp/setup_pipelines.sh
@@ -45,6 +45,16 @@ git switch --detach v0.2.0
 git apply ../../pipelines/genz_icp.patch
 cd ..
 
+# MAD-ICP
+if [ ! -d "mad-icp" ]; then
+    git clone git@github.com:rvp-group/mad-icp.git
+fi
+cd mad-icp
+git stash
+git switch --detach v0.0.10
+git apply ../../pipelines/mad_icp.patch
+cd ..
+
 # ------------------------- Dependencies ------------------------- #
 cd $topdir
 if [ ! -d ".vcpkg/" ]; then
