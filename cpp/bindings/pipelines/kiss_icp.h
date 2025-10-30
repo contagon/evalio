c629fef700b043b56a9e0abe528ccd29a2d2b0b7
Easton Potokar <contagon6@gmail.com>
2025-10-30 16:39:17 -0400

    feat(pl): Type conversions module (#62)
    * Add in conversion module
    * Got vector conversions working as well!
    * Do it to genz as well
    * Clean up a few more pipelines with new conversions
    * Move all of binding conversion over to new convert function
    * Move conversions of iters and maps to separate methods

--- cpp/bindings/pipelines/kiss_icp.h
+++ cpp/bindings/pipelines/kiss_icp.h
@@ -1,41 +1,16 @@
 #pragma once
 
 #include <memory>
-#include <stdexcept>
 
+#include "evalio/convert/eigen.h"
+#include "evalio/convert/sophus.h"
 #include "evalio/pipeline.h"
 #include "evalio/types.h"
 #include "kiss_icp/pipeline/KissICP.hpp"
 
-inline evalio::Point to_evalio_point(Eigen::Vector3d point) {
-  return {
-    .x = point[0],
-    .y = point[1],
-    .z = point[2],
-    .intensity = 0.0,
-    .t = evalio::Duration::from_sec(0),
-    .row = 0,
-    .col = 0
-  };
-}
+namespace ev = evalio;
 
-inline Eigen::Vector3d to_eigen_point(evalio::Point point) {
-  return {point.x, point.y, point.z};
-}
-
-inline evalio::SE3 to_evalio_se3(Sophus::SE3d pose) {
-  const auto t = pose.translation();
-  const auto q = pose.unit_quaternion();
-  const auto rot =
-    evalio::SO3 {.qx = q.x(), .qy = q.y(), .qz = q.z(), .qw = q.w()};
-  return evalio::SE3(rot, t);
-}
-
-inline Sophus::SE3d to_sophus_se3(evalio::SE3 pose) {
-  return Sophus::SE3d(Sophus::SO3d(pose.rot.toEigen()), pose.trans);
-}
-
-class KissICP: public evalio::Pipeline {
+class KissICP: public ev::Pipeline {
 public:
   KissICP() : config_() {}
 
@@ -64,31 +39,27 @@ public:
   );
 
   // Getters
-  const evalio::SE3 pose() override {
+  const ev::SE3 pose() override {
     const Sophus::SE3d pose = kiss_icp_->pose() * lidar_T_imu_;
-    return to_evalio_se3(pose);
+    return ev::convert<ev::SE3>(pose);
   }
 
-  const std::map<std::string, std::vector<evalio::Point>> map() override {
-    std::vector<Eigen::Vector3d> map = kiss_icp_->LocalMap();
-    std::vector<evalio::Point> evalio_map;
-    evalio_map.reserve(map.size());
-    for (auto point : map) {
-      evalio_map.push_back(to_evalio_point(point));
-    }
-    return {{"point", evalio_map}};
+  const std::map<std::string, std::vector<ev::Point>> map() override {
+    return ev::convert_map<std::vector<Eigen::Vector3d>>(
+      {{"point", kiss_icp_->LocalMap()}}
+    );
   }
 
   // Setters
-  void set_imu_params(evalio::ImuParams params) override {}
+  void set_imu_params(ev::ImuParams params) override {}
 
-  void set_lidar_params(evalio::LidarParams params) override {
+  void set_lidar_params(ev::LidarParams params) override {
     config_.max_range = params.max_range;
     config_.min_range = params.min_range;
   }
 
-  void set_imu_T_lidar(evalio::SE3 T) override {
-    lidar_T_imu_ = to_sophus_se3(T).inverse();
+  void set_imu_T_lidar(ev::SE3 T) override {
+    lidar_T_imu_ = ev::convert<Sophus::SE3d>(T).inverse();
   }
 
   // Doers
@@ -96,30 +67,21 @@ public:
     kiss_icp_ = std::make_unique<kiss_icp::pipeline::KissICP>(config_);
   }
 
-  void add_imu(evalio::ImuMeasurement mm) override {}
+  void add_imu(ev::ImuMeasurement mm) override {}
 
-  std::map<std::string, std::vector<evalio::Point>>
-  add_lidar(evalio::LidarMeasurement mm) override {
-    // Set everything up
-    std::vector<Eigen::Vector3d> points;
-    points.reserve(mm.points.size());
-    std::vector<double> timestamps;
-    timestamps.reserve(mm.points.size());
-
-    // Copy
-    for (auto point : mm.points) {
-      points.push_back(to_eigen_point(point));
-      timestamps.push_back(point.t.to_sec());
-    }
+  std::map<std::string, std::vector<ev::Point>>
+  add_lidar(ev::LidarMeasurement mm) override {
+    // Convert inputs
+    auto points = ev::convert_iter<std::vector<Eigen::Vector3d>>(mm.points);
+    auto timestamps = ev::convert_iter<std::vector<double>>(mm.points);
 
     // Run through pipeline
     const auto& [_, used_points] = kiss_icp_->RegisterFrame(points, timestamps);
-    std::vector<evalio::Point> result;
-    result.reserve(used_points.size());
-    for (auto point : used_points) {
-      result.push_back(to_evalio_point(point));
-    }
-    return {{"point", result}};
+
+    // Convert outputs
+    return ev::convert_map<std::vector<Eigen::Vector3d>>(
+      {{"point", used_points}}
+    );
   }
 
 private:
---
+++ cpp/evalio/convert/base.h
@@ -0,0 +1,63 @@
+// Helper module for converting between the various library types.
+#pragma once
+
+#include <map>
+
+#include "evalio/types.h"
+
+namespace evalio {
+
+/// @brief Generic conversion function between types.
+///
+/// Overloads and specializations may be imported from other files or defined by the user.
+template<typename Out, typename In>
+inline Out convert(const In& in) = delete;
+
+/// @brief Convert evalio::Point to timestamps as doubles.
+template<>
+inline double convert(const evalio::Point& in) {
+  return in.t.to_sec();
+}
+
+/// @brief Convert a container of In to a container of Out using convert<Out>(in).
+///
+/// Assumes the following are defined:
+/// - In::size().
+/// - In is iterable with range-based for loops.
+/// - Out is default-constructible.
+/// - Out::value_type.
+/// - Out::reserve(size_t).
+/// - Out::push_back(Out::value_type).
+/// - convert<Out::value_type, In::value_type>(In::value_type).
+
+template<class Out, class In>
+inline Out convert_iter(const In& in) {
+  Out out;
+  out.reserve(in.size());
+  for (const auto& item : in) {
+    out.push_back(
+      convert<typename Out::value_type, typename In::value_type>(item)
+    );
+  }
+  return out;
+}
+
+/// @brief Convert a map of arbitrary containers to a map of std::vector<evalio::Point>.
+///
+/// Useful for converting to final evalio map types. Unlike other convert functions,
+/// this one only requires the input type to be specified.
+/// Assumes the following are defined:
+/// - In::size().
+/// - In is iterable with range-based for loops.
+/// - convert<evalio::Point, In::value_type>(In::value_type).
+template<class In>
+inline std::map<std::string, std::vector<evalio::Point>>
+convert_map(const std::map<std::string, In>& in) {
+  std::map<std::string, std::vector<evalio::Point>> out;
+  for (const auto& [key, val] : in) {
+    out[key] = convert_iter<std::vector<evalio::Point>>(val);
+  }
+  return out;
+}
+
+} // namespace evalio
